9b849a3a64965927e90963d3d879a55c9712b6e1
Charlin Agramonte <char0394@gmail.com>
2019-05-14 15:55:01 -0400

    Adding project

---
+++ MarkupExtensionSample/Extensions/FontSizeExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MarkupExtensionSample.Extensions
+{
+    public class FontSizeExtension : IMarkupExtension<double>
+    {
+        public Button Button { set; get; }
+      
+        public double ProvideValue(IServiceProvider serviceProvider)
+        {
+            if(Button!=null)
+             return Button.HeightRequest / 2;
+
+            return 0;
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return (this as IMarkupExtension<double>).ProvideValue(serviceProvider);
+        }
+    }
+}
